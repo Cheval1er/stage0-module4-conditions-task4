a0ee22793f6485fa92a99641c83c89ff60431949
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2023-12-11 12:08:19 +0400

    Module #4.1. Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int b=0 ;
+        b = b < first ? first : second;
+        System.out.println(b);
     }
 }
